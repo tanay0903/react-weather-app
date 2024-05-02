d0403223dabe9a33e3e1c8826c6382af8aefba43
tanay0903 <tanaykumar0903@gmail.com>
2024-05-03 03:27:19 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "33a4d298a5mshe9de027e356b1a1p1cdb81jsnfc6a889af9e2",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "a2dde2f665212f63fa6713bed7acd3b0"; // enter your key from openweather API
